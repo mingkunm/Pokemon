4e8a4e761c9fe46e09396e7852868705aea1ee58
mingkunma <mmk.ee.911@gmail.com>
2021-04-19 03:05:20 -0700

    feat: Add mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 
 const main = require("./routes/example");
+const connectMongoDB = require("./db/mongodb");
+
+connectMongoDB();
 
 app.use(express.json());
 app.use(cors());
