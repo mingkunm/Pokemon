2c4efd887451fd12eb06b5a6e216de7bbbb17d7e
mingkunma <mmk.ee.911@gmail.com>
2021-04-21 00:41:35 -0700

    fix: Cannot get home page after deploy

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // connectMongoDB();
 
