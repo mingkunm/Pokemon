022fe14f0a7866fb84d24067d33199aead20547a
mingkunma <mmk.ee.911@gmail.com>
2021-04-21 00:49:40 -0700

    Revert "fix: Cannot get home page after deploy"
    This reverts commit 2c4efd887451fd12eb06b5a6e216de7bbbb17d7e.

--- server/server.js
+++ server/server.js
@@ -9,9 +9,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
 // connectMongoDB();
 
