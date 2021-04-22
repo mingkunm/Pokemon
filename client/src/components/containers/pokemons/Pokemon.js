13a4138f1d973522be6f0c74a008b48a1cc61574
mingkunma <mmk.ee.911@gmail.com>
2021-04-22 02:52:26 -0700

    feat: Add pokemon avatar

--- client/src/components/containers/pokemons/Pokemon.js
+++ client/src/components/containers/pokemons/Pokemon.js
@@ -5,9 +5,20 @@ function Pokemon({ pokemon }) {
 
   return (
     <div className="pokemon">
-      <h5>Name: {name}</h5>
-      <h5>Type: {type}</h5>
-      <h5>Move: {move}</h5>
+      <div className="avatar-container">
+        <img
+          src={`https://img.pokemondb.net/artwork/${
+            name.charAt(0).toLowerCase() + name.slice(1)
+          }.jpg`}
+          alt={`${name} img`}
+          className="avatar"
+        />
+      </div>
+      <div>
+        <h5>Name: {name}</h5>
+        <h5>Type: {type}</h5>
+        <h5>Move: {move}</h5>
+      </div>
     </div>
   );
 }
